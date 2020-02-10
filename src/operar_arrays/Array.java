9da4eebd3e81fc774a2ba101632370bd77994977
antonioluqueparlon <antonioluquepar@gmail.com>
2020-02-10 09:59:04 +0100

    actualizado

--- src/operar_arrays/Array.java
+++ src/operar_arrays/Array.java
@@ -1,21 +1,74 @@
 package operar_arrays;
 
-
 public class Array {
-	 
-	public static void rellena_array() {
-		int array[]= new int [10];
-		//Inicializamos los valores del array y declaramos que sean numeros al azar
-		for(int i=0; i<array.length; i++) {
-		array[i]=Utils.obtenerNumeroAzar(1,100);
-		}
-	}
 
 	public static void main(String[] args) {
-		
-		rellena_array();
-	
+		// TODO Auto-generated method stub
+		int suma= 0;
+		// Declaración de array
+				int array[] = rellena_arrays(10, 1, 20);
+
+		// Mostramos el array
+				mostrarArray(array);
+
+		//	Metodo sumar elementos del array
+				sumar_array(array, suma);
 	
 	}
-}
+			
+
+			/**
+			 *
+			 * @param longitud
+			 * @param limInf
+			 * @param limSup
+			 * @return
+			 */
+			public static int[] rellena_arrays(int longitud, int limInf, int limSup) {
+				int array[] = new int[longitud];
+				for (int i = 0; i < array.length; i++) {
+					array[i] = Utils.obtenerNumeroAzar(limInf, limSup);
+				}
+				return array;
+			}
+
+			/**
+			 *
+			 * @param array
+			 */
+			public static void mostrarArray(int[] array) {
+				System.out.println("Contenido del array");
+				for (int i = 0; i < array.length; i++) {
+					System.out.print(array[i] + " ");
+				}
+			}
+			
+			public static void sumar_array(int[] array, int suma) {
+				for (int numero : array) {
+
+					suma += numero;
+				}
+				// Imprimo el resultado
+				System.out.println("\nSuma elementos del array: " + suma );
+
+			}
+
+			/**
+			 *
+			 * @return
+			 */
+			private static int obtenerNumeroAleatorio() {
+				return (int) Math.round(Math.random() * 100);
+			}
+
+			/**
+			 *
+			 * @return
+			 */
+			private static int obtenerNumeroAleatorio(int limiteInferior, int limiteSuperior) {
+				return (int) Math.round(Math.random() * (limiteSuperior - limiteInferior) + limiteInferior);
+			}
+
+	}
+
 
